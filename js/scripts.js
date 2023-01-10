a890982e656a73ff1fba3addba32c3ea91af72b7
Zsolt Dzsugán <zsoltd.business@gmail.com>
2023-01-10 23:53:35 +0100

    restart

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,7 @@
-/*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+const light = document.querySelector(".light");
+const hexGrid = document.querySelector("#hex-grid");
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
+hexGrid.addEventListener("mousemove", function (e) {
+    light.style.left = `${e.clientX}px`;
+    light.style.top = `${e.clientY}px`;
 });
